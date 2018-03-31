d76009a12e4bd7d469f8521734735d652aae3d50
yassine <yechabbi@gmail.com>
2018-03-31 22:03:17 +0100

    0.1.1 Release

--- pom.groovy
+++ pom.groovy
@@ -3,7 +3,7 @@ project {
   modelVersion '4.0.0'
   groupId 'com.github.yassine.artifacts'
   artifactId 'guice-artifacts'
-  version '0.1.0'
+  version '0.1.1'
 
   properties {
     'deps.version.spock' '1.1-groovy-2.4'
